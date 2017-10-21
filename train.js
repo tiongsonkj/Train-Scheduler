fde4a34bdead5415f065f008d0c1ba7277111790
Kelvin Tiongson <kelvintiongson@kelvins-mbp-2.attlocal.net>
2017-10-20 21:34:00 -0500

    first commit html page

---
+++ train.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBQ6c2E5hJrzZwI_TCCedBLo_2M0kGlbXE",
+authDomain: "train-schedule-homework-f47d8.firebaseapp.com",
+databaseURL: "https://train-schedule-homework-f47d8.firebaseio.com",
+projectId: "train-schedule-homework-f47d8",
+storageBucket: "train-schedule-homework-f47d8.appspot.com",
+messagingSenderId: "625740817469"
+};
+firebase.initializeApp(config);
+
